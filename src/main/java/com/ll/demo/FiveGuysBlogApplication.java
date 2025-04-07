d25b8a327218d79fafd37e33f32210468ba58ce9
thatisbummer <tjdals4358@naver.com>
2025-04-07 17:09:06 +0900

    1

---
+++ src/main/java/com/ll/demo/FiveGuysBlogApplication.java
@@ -0,0 +1,13 @@
+package com.ll.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiveGuysBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiveGuysBlogApplication.class, args);
+	}
+
+}
